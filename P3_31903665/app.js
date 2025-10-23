d0feb4c9006fad5ebf2c26822f6cb382aa7f2be2
Junyelmer <gabrielandres0111@gmail.com>
2025-10-23 18:14:57 -0400

    fix: Eliminada configuracion incorrecta de better-sqlite3

--- P3_31903665/app.js
+++ P3_31903665/app.js
@@ -43,8 +43,8 @@ const usersRouter = require(path.join(__dirname, 'routes', 'users')); // <-- aho
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' })); // <-- ahora robusto
+app.use(express.urlencoded({ extended: false, limit: '10mb' })); // <-- ahora robusto
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
